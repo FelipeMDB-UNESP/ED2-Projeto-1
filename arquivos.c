2d813fab2b63733e845bce91a3f090367c7af221
FelipeMDB <>
2023-09-17 17:43:44 -0300

    Update arquivos.c
    Cria função para carregar os registros num vetor

--- arquivos.c
+++ arquivos.c
@@ -5,17 +5,20 @@
 
 // Definindo a struct
 struct Registro {
-    char codClie[11];
-    char codVei[7];
-    char chavePri[50];
+    char codClie[12];
+    char codVei[8];
     char nomeCliente[50];
     char nomeVeiculo[50];
     int qtdDias;
 };
 
+void incluir(struct Registro registro) {
 
-void incluir(){
+    FILE* registros = fopen("insere.bin", "rb+");
 
+    fwrite(&registros, sizeof(struct Registro), 1, registros);
+    fclose(registros);
+    
 }
 
 void remover(){
@@ -26,8 +29,45 @@ void compactacao(){
 
 }
 
-void carregar(){
 
+struct Registro* carregar(int* numRegistros) {
+    FILE* arquivo = fopen("insere.bin", "rb+");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo");
+        exit(1);
+    }
+
+    //tam arquivo
+    fseek(arquivo, 0, SEEK_END);
+    long tamanhoArquivo = ftell(arquivo);
+    rewind(arquivo);
+
+    // calcular numRegistros
+    *numRegistros = tamanhoArquivo / sizeof(struct Registro);
+
+    // Aloca de memória
+    struct Registro* vetorRegistros = (struct Registro*)malloc(*numRegistros * sizeof(struct Registro));
+
+    //Lê registros
+    fread(vetorRegistros, sizeof(struct Registro), *numRegistros, arquivo);
+
+    // Fecha o arquivo
+    fclose(arquivo);
+
+    return vetorRegistros;
+}
+
+
+void imprimirRegistros(struct Registro* vetorRegistros, int numRegistros) {
+    for (int i = 0; i < numRegistros; i++) {
+        printf("Registro %d:\n", i + 1);
+        printf("Código do Cliente: %s\n", vetorRegistros[i].codClie);
+        printf("Código do Veículo: %s\n", vetorRegistros[i].codVei);
+        printf("Nome do Cliente: %s\n", vetorRegistros[i].nomeCliente);
+        printf("Nome do Veículo: %s\n", vetorRegistros[i].nomeVeiculo);
+        printf("Quantidade de Dias: %d\n", vetorRegistros[i].qtdDias);
+        printf("\n");
+    }
 }
 
 
@@ -45,6 +85,10 @@ int main() {
     // strcpy(r1.chavePri, "73849561829");
     // strcat(r1.chavePri, l1.codVei);
 
+    int numRegistros = 0;
+    struct Registro* vetorRegistro = carregar(&numRegistros);
+    imprimirRegistros(vetorRegistro, numRegistros);
+
 
     int opcao;
     do {
@@ -61,19 +105,19 @@ int main() {
         switch (opcao) {
             case 1:
                 printf("Realizando a inserção:\n");
-                incluir();
+                //incluir();
                 break;
             case 2:
                 printf("Realizando a remoção:\n");
-                remover();
+                //remover();
                 break;
             case 3:
                 printf("Compactação:\n");
-                compactacao();
+                //compactacao();
                 break;
             case 4:
                 printf("Carregando arquivo:\n");
-                carregar();
+                //carregar();
                 break;
             case 0:
                 printf("Execução encerrada!\n");
@@ -82,7 +126,7 @@ int main() {
                 printf("Opção inválida. Por favor, escolha uma opção válida (0-4).\n");
                 break;
         }
-    } while (opcao != 0);
+    } while (opcao != 0);   
 
 
 
